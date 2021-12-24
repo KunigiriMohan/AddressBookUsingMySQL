6f9bcb94d1891dfea3925f0498fde6d945d0bc22
KunigiMohan <kmohanr5@gmail.com>
2021-12-24 22:04:30 +0530

    Added : AddressBookApplication main class.

---
+++ src/main/java/com/application/addressbook/AddressBookApplication.java
@@ -0,0 +1,15 @@
+package com.application.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @SpringBootApplication : main Class of Application
+ */
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplication.class, args);
+	}
+
+}
